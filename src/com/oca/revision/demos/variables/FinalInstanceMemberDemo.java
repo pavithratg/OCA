ce57cc9885b184c669e8c91c96a799b0f2d85efb
pavithra <thilini.gunasekara@gmail.com>
2016-01-26 20:23:29 -0800

    01/26/2016 changes

--- src/com/oca/revision/demos/variables/FinalInstanceMemberDemo.java
+++ src/com/oca/revision/demos/variables/FinalInstanceMemberDemo.java
@@ -11,24 +11,28 @@ public class FinalInstanceMemberDemo {
 	 */
 
 	/*
-	 * Second way is to initialze the final instance variable : within a
+	 * Second way is to initialize the final instance variable : within a
 	 * constructor public FinalInstanceMemberDemo(int a) { this.a = a; }
 	 */
 
 	/*
-	 * Third way to initilize a final instance variable : within initialization
+	 * Third way to initialize a final instance variable : within initialization
 	 * blocks
 	 */
 	{
 		this.a = 10;
 	}
-	
-	// Final instance variable initialization cannot be happened within a static block.
-	static{
-		
+
+	// Final instance variable initialization cannot be happened within a static
+	// block, since the class level is not aware of the object, final variable
+	// belongs to.
+	static {
+
 	}
 
-	// Final instance variable initialization cannot be happened within a method.
+	// Final instance variable initialization cannot be happened within a
+	// method. It should be happened before the constructor completes, therefore
+	// initialization cannot be taken place in a method.
 	void initialize(int a) {
 		// this.a = a;
 	}
