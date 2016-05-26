cc909e4e3c071a1a6eefc1cf424aae08cb8222bf
pavithra <thilini.gunasekara@gmail.com>
2016-05-26 13:35:40 -0700

    [05/26/2016]

--- src/com/oca/revision/demos/misc/OperatorPrecedenceGrouping.java
+++ src/com/oca/revision/demos/misc/OperatorPrecedenceGrouping.java
@@ -81,6 +81,26 @@ public class OperatorPrecedenceGrouping {
 		k += (k = 4) * (k + 2);
 		System.out.println("k: " + k);
 
+		// Example 5:
+		boolean b1 = false;
+		int i1 = 2;
+		int i2 = 3;
+
+		/*
+		 * The expression b1 = i1 == i2 will be evaluated as b1 = (i1 == i2)
+		 * because == has higher precedence than =. Further, all an if statement
+		 * needs is a boolean. Now i1 == i2 returns false which is a boolean and
+		 * since b1 = false is an expression and every expression has a return
+		 * value (which is actually the Left Hand Side of the expression), it
+		 * returns false which is again a boolean. Therefore, in this case, the
+		 * else condition will be executed.
+		 */
+		if (b1 = i1 == i2) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+
 	}
 
 }
